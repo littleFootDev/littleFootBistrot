983e50eec43c871c5eb949c07affb6ea398a0b67
littleFootDev <thelittlefootdev@gmail.com>
2022-02-17 18:29:02 +0100

    start restaurant page

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-const btnMenu = document.querySelector(".btn-rond-menu");
+
 const btnMenu2 = document.querySelector(".btn-rond-menu2");
-const nav = document.querySelector(".nav");
+const nav = document.querySelector(".nav-restaurant");
 const allItemNav = document.querySelectorAll(".nav-menu-item");
 const ligne = document.querySelector(".cont-ligne");
 
-btnMenu.addEventListener("click", () => {
+btnMenu2.addEventListener("click", () => {
+    console.log('click');
     ligne.classList.toggle("active");
-    nav.classList.toggle("menu-visible");
+    nav.classList.toggle("menu-visible2");
 });
 
 
@@ -14,7 +15,7 @@ if (window.matchMedia("max-width:1300px")) {
     allItemNav.forEach((item) => {
       item.addEventListener("click", () => {
         console.log(item);
-        nav.classList.toggle("menu-visible");
+        nav.classList.toggle("menu-visible2");
         ligne.classList.toggle("active");
       });
     });
