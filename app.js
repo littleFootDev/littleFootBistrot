fdf1b08654a813af8c3992a1c890137d2c03cc7f
littleFootDev <thelittlefootdev@gmail.com>
2022-03-04 16:51:39 +0100

    refactorisation and new design

--- app.js
+++ app.js
@@ -20,3 +20,37 @@ if (window.matchMedia("max-width:1300px")) {
       });
     });
   }
+
+const items = document.querySelectorAll('.img-carousel');
+const nbsSlide  = items.length;
+const suivant = document.querySelector('.right');
+const precedent = document.querySelector('.left');
+
+let count = 0;
+
+
+suivant.addEventListener("click", () => {
+  items[count].classList.remove('active');
+
+  if(count < nbsSlide - 1) {
+    count++;
+  } else {
+    count = 0;
+  }
+
+  items[count].classList.add('active');
+})
+
+
+precedent.addEventListener("click", (e) =>{
+  items[count].classList.remove('active');
+
+  if(count > 0) {
+    count--;
+  } else {
+    count = nbsSlide - 1
+  }
+
+  items[count].classList.add('active');
+  
+});
